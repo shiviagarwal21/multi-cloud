ede9d10b84e98453445c1368d69aa4a4efef773b
shiviagarwal21 <46994484+shiviagarwal21@users.noreply.github.com>
2020-06-15 17:07:11 +0530

    commit-1

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1 style="color:gray;">Welcome!</h1>
+<p> 
+Terraform is a tool for building, changing, and versioning infrastructure safely and efficiently.
+Terraform can manage existing and popular service providers as well as custom in-house solutions.
+Configuration files describe to Terraform the components needed to run a single application or your entire datacenter.
+Terraform generates an execution plan describing what it will do to reach the desired state, and then executes it to build
+the described infrastructure. As the configuration changes, Terraform is able to determine what changed and create incremental
+execution plans which can be applied.
+The infrastructure Terraform can manage includes low-level components such as compute instances, 
+storage, and networking, as well as high-level components such as DNS entries, SaaS features, etc.
+</p>
+<img src = https://shivi-cloud-task08.s3.ap-south-1.amazonaws.com/image1.png  width="500" height="600">
+<br/>
+
+<pre>
+<?php
+print `ifconfig`;
+?>
+</pre>
+</body>
+</html>
